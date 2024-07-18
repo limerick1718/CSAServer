4bbab98dcefb4d6e13b3836217626c2ce30203e1
Jiakun <limerick1718@outlook.com>
2024-07-18 16:49:19 +0700

    authentication added

--- tests/login_tests.py
+++ tests/login_tests.py
@@ -3,7 +3,19 @@ from main import app
 
 client = TestClient(app)
 
-from fastapi.testclient import TestClient
-from main import app
-
-client = TestClient(app)
+def test_register():
+    user = {
+        "username": "test",
+        "email": "test@qwq.com",
+        "password": "asdfa"
+    }
+    request = client.build_request(
+        method="post",
+        url="/register",
+        data=user
+    )
+    response = client.send(request)
+    result = response.json()
+    detail = result["detail"]
+    assert detail == "Email already registered"
+    assert response.status_code == 200
